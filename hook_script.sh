86da176e486a8b5dd69a830d8660f6b9c526e95c
Ian Murray <imurraycoding@gmail.com>
2023-08-03 20:37:25 +0100

    update hook script to pull repo

--- hook_script.sh
+++ hook_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Hook script running..."
+echo "Starting git pull"
+git pull origin main
+echo "Git pull complete"
 echo "Starting upload script..."
 ./upload_script.sh
 echo "Done with upload script..."
